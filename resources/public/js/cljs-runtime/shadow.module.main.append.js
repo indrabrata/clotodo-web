ada9ede66d62776e6bdbc2c64815329e6ab9184d
Indra Brata <112854205+indrabrata@users.noreply.github.com>
2025-10-06 20:48:04 +0700

    chore : delete resources/public/js directory

--- resources/public/js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { clotodo_web.core.init_BANG_(); } catch (e) { console.error("An error occurred when calling (clotodo-web.core/init!)"); console.error(e); }
